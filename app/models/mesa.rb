cc67b0231b2924547cf23de9a1b19e5f31c1d121
Augusto <augusto455@gmail.com>
2014-09-11 15:20:01 -0300

    rails

---
+++ app/models/mesa.rb
@@ -0,0 +1,3 @@
+class Mesa < ActiveRecord::Base
+  attr_accessible :c_mesa, :c_user, :n_user
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mock::Application.config.secret_token = '20c021df95c443973fbab5ac5f25375d03bdbe6325fca98149d1143153e98d94df8d6a0d14908a131175806b9177b994d618394bf44990bbcedd9f6304d81e41'
